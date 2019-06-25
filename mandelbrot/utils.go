58e6d6ca9eaa06b50c83ae55891745e14f895320
HakanSunay <hakansunay@abv.bg>
2019-06-25 23:01:32 +0300

    Handle incorrect input with default values

--- mandelbrot/utils.go
+++ mandelbrot/utils.go
@@ -25,23 +25,26 @@ func CreatePixelMatrix(h int, w int) [][]color.NRGBA {
 
 func GetRanges(s string) (float64, float64, float64, float64) {
 	ranges := strings.Split(s, ":")
-	rmin := parseFloat(ranges[0], DefaultRealMin)
-	rmax := parseFloat(ranges[1], DefaultRealMax)
-	imin := parseFloat(ranges[2], DefaultImagMin)
-	imax := parseFloat(ranges[3], DefaultImagMax)
+	rmin := parseFloat(ranges, 0, DefaultRealMin)
+	rmax := parseFloat(ranges, 1, DefaultRealMax)
+	imin := parseFloat(ranges,2, DefaultImagMin)
+	imax := parseFloat(ranges,3, DefaultImagMax)
 	return rmin, rmax, imin, imax
 
 }
 
 func GetDimensions(s string) (int, int) {
 	dimensions := strings.Split(s, "x")
-	width := int(parseFloat(dimensions[0], DefaultWIDTH))
-	height := int(parseFloat(dimensions[1], DefaultHEIGHT))
+	width := int(parseFloat(dimensions, 0,  DefaultWIDTH))
+	height := int(parseFloat(dimensions, 1, DefaultHEIGHT))
 	return width, height
 }
 
-func parseFloat(value string, defaultValue float64) float64 {
-	result, err := strconv.ParseFloat(value, 64)
+func parseFloat(values []string, soughtIndexOfValue int, defaultValue float64) float64 {
+	if indexDoesntExist(len(values), soughtIndexOfValue) {
+		return defaultValue
+	}
+	result, err := strconv.ParseFloat(values[soughtIndexOfValue], 64)
 	if err != nil {
 		result = defaultValue
 	}
@@ -52,4 +55,11 @@ func FillChannelWithColumns(c *chan int, width int) {
 	for i := 0; i < width; i++ {
 		*c <- i
 	}
+}
+
+func indexDoesntExist(length int, index int) bool{
+	if length < index + 1 {
+		return true
+	}
+	return false
 }
