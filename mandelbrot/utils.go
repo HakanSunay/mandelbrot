5787d10be56a4cc5b2957ca16eda431474963e05
HakanSunay <hakansunay@abv.bg>
2019-06-25 23:02:38 +0300

    Rearrange function order

--- mandelbrot/utils.go
+++ mandelbrot/utils.go
@@ -23,19 +23,25 @@ func CreatePixelMatrix(h int, w int) [][]color.NRGBA {
 	return pixels
 }
 
+func FillChannelWithColumns(c *chan int, width int) {
+	for i := 0; i < width; i++ {
+		*c <- i
+	}
+}
+
 func GetRanges(s string) (float64, float64, float64, float64) {
 	ranges := strings.Split(s, ":")
 	rmin := parseFloat(ranges, 0, DefaultRealMin)
 	rmax := parseFloat(ranges, 1, DefaultRealMax)
-	imin := parseFloat(ranges,2, DefaultImagMin)
-	imax := parseFloat(ranges,3, DefaultImagMax)
+	imin := parseFloat(ranges, 2, DefaultImagMin)
+	imax := parseFloat(ranges, 3, DefaultImagMax)
 	return rmin, rmax, imin, imax
 
 }
 
 func GetDimensions(s string) (int, int) {
 	dimensions := strings.Split(s, "x")
-	width := int(parseFloat(dimensions, 0,  DefaultWIDTH))
+	width := int(parseFloat(dimensions, 0, DefaultWIDTH))
 	height := int(parseFloat(dimensions, 1, DefaultHEIGHT))
 	return width, height
 }
@@ -51,14 +57,8 @@ func parseFloat(values []string, soughtIndexOfValue int, defaultValue float64) f
 	return result
 }
 
-func FillChannelWithColumns(c *chan int, width int) {
-	for i := 0; i < width; i++ {
-		*c <- i
-	}
-}
-
-func indexDoesntExist(length int, index int) bool{
-	if length < index + 1 {
+func indexDoesntExist(length int, index int) bool {
+	if length < index+1 {
 		return true
 	}
 	return false
