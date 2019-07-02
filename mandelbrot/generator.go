b964df1104e9d2793ef5b12fae82d67228f029f9
HakanSunay <hakansunay@abv.bg>
2019-07-02 13:49:54 +0300

    Document FractalGenerator methods for godoc

--- mandelbrot/generator.go
+++ mandelbrot/generator.go
@@ -15,16 +15,23 @@ const (
 	AlphaAmplifier      = 255
 )
 
+// Fractal Generator is a manager struct that helps us
+// perform the main operations when creating the fractal
 type FractalGenerator struct {
 	picture   Picture
 	bounds    Bound
 	algorithm Algorithm
 }
 
+// NewFractalGenerator initializes a new FractalGenerator struct
+// using the passed in components
 func NewFractalGenerator(picture Picture, bound Bound, algorithm Algorithm) *FractalGenerator {
 	return &FractalGenerator{picture: picture, bounds: bound, algorithm: algorithm}
 }
 
+// StartComputation generates the fractal image
+// it uses a WaitGroup and a Channel to time and
+// paralyze the mathematical computation of the mandelbrot set
 func (fg *FractalGenerator) StartComputation(w *sync.WaitGroup, channel *chan int) {
 	for n := 0; n < fg.getWorkerCount(); n++ {
 		w.Add(1)
@@ -32,6 +39,9 @@ func (fg *FractalGenerator) StartComputation(w *sync.WaitGroup, channel *chan in
 	}
 }
 
+// computeAvailableRow ranges the passed in channel and
+// when given a certain number, it generates the full
+// mandelbrot picture in the row corresponding to the given number
 func (fg *FractalGenerator) computeAvailableRow(w *sync.WaitGroup, channel *chan int) {
 	pixelMatrix := fg.getPixelMatrix()
 	for x := range *channel {
@@ -51,6 +61,10 @@ func (fg *FractalGenerator) computeAvailableRow(w *sync.WaitGroup, channel *chan
 	w.Done()
 }
 
+// ExportImage is used to export the generated image
+// in the previous process started by StartComputation
+// if it is called before StartComputation
+// it will generate an empty image
 func (fg *FractalGenerator) ExportImage() *image.NRGBA {
 	img := image.NewNRGBA(image.Rect(0, 0, fg.getWidth(), fg.getHeight()))
 	colorMatrix := fg.getPixelMatrix()
@@ -62,6 +76,8 @@ func (fg *FractalGenerator) ExportImage() *image.NRGBA {
 	return img
 }
 
+// belongsToMandelbrotSet checks if the iterations of a given
+// coordinate belongs to the Mandelbrot set
 func (fg *FractalGenerator) belongsToMandelbrotSet(iterations uint8) bool {
 	if iterations < fg.maxIterations() {
 		return false
@@ -70,30 +86,41 @@ func (fg *FractalGenerator) belongsToMandelbrotSet(iterations uint8) bool {
 	}
 }
 
+// pixelToComplex converts a pixel represented by x and y coordinates
+// into a number in the Complex plane - having real and imaginary coordinates
 func (fg *FractalGenerator) pixelToComplex(x, y int) complex128 {
 	return fg.picture.pixelToComplex(x, y, fg.bounds)
 }
 
+// computeIterations calls the algorithm to get the iterations
+// of a particular complex number
 func (fg *FractalGenerator) computeIterations(num complex128) uint8 {
 	return fg.algorithm.getIterations(num)
 }
 
+// maxIterations returns the limit beyond which a point
+// is considered inside the Mandelbrot set
 func (fg *FractalGenerator) maxIterations() uint8 {
 	return fg.algorithm.maxIterations
 }
 
+// getPixelMatrix returns the image represented as a pixel matrix
 func (fg *FractalGenerator) getPixelMatrix() [][]color.NRGBA {
 	return fg.picture.pixelMatrix
 }
 
+// getWorkerCount provides us with the number of threads specified
+// by the user of the app
 func (fg *FractalGenerator) getWorkerCount() int {
 	return fg.algorithm.workers
 }
 
+// getWidth provides us with the width of the desired image
 func (fg *FractalGenerator) getWidth() int {
 	return fg.picture.width
 }
 
+// getHeight provides us with the height of the desired image
 func (fg *FractalGenerator) getHeight() int {
 	return fg.picture.height
 }
