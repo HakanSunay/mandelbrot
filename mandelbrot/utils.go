2c503d9329153177e809d303d5230cc09af6f44e
HakanSunay <hakansunay@abv.bg>
2019-07-02 14:02:03 +0300

    Document utility functions for godoc

--- mandelbrot/utils.go
+++ mandelbrot/utils.go
@@ -15,6 +15,8 @@ const (
 	DefaultRealMax = 2.
 )
 
+// CreatePixelMatrix initializes a 2d array of type color.NRGBA
+// for its height and width uses the user-given parameters
 func CreatePixelMatrix(h int, w int) [][]color.NRGBA {
 	pixels := make([][]color.NRGBA, w)
 	for r := range pixels {
@@ -23,12 +25,15 @@ func CreatePixelMatrix(h int, w int) [][]color.NRGBA {
 	return pixels
 }
 
+// FillChannelWithRows loops through the numbers
+// from 0 to width - 1 and sends them to a given channel
 func FillChannelWithRows(c *chan int, width int) {
 	for i := 0; i < width; i++ {
 		*c <- i
 	}
 }
 
+// GetRanges extracts the bounds from the input of the user
 func GetRanges(s string) (float64, float64, float64, float64) {
 	ranges := strings.Split(s, ":")
 	rmin := parseFloat(ranges, 0, DefaultRealMin)
@@ -38,6 +43,7 @@ func GetRanges(s string) (float64, float64, float64, float64) {
 	return rmin, rmax, imin, imax
 }
 
+// GetDimensions extracts the width and the height from the input of the user
 func GetDimensions(s string) (int, int) {
 	dimensions := strings.Split(s, "x")
 	width := int(parseFloat(dimensions, 0, DefaultWIDTH))
@@ -45,6 +51,8 @@ func GetDimensions(s string) (int, int) {
 	return width, height
 }
 
+// parseFloat is used to eliminate DRY when parsing numbers from string
+// if the soughtIndex is out of bounds a default value is provided
 func parseFloat(values []string, soughtIndexOfValue int, defaultValue float64) float64 {
 	if indexDoesntExist(len(values), soughtIndexOfValue) {
 		return defaultValue
@@ -56,6 +64,7 @@ func parseFloat(values []string, soughtIndexOfValue int, defaultValue float64) f
 	return result
 }
 
+// indexDoesntExist checks if the sought index is out of bounds
 func indexDoesntExist(length int, index int) bool {
 	if length < index+1 {
 		return true
